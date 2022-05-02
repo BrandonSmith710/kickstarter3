b188f2ec997be2cb114654ee3e7a6cda0202fe3e
BrandonSmith710 <75755695+BrandonSmith710@users.noreply.github.com>
2022-05-02 01:21:34 -0700

    Update model.py

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ model.compile(model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['mse']))
 
-#given imputs take test df
+# given imputs take test df
 def user_input(user_campaign):
     X_features = user_campaign[['launch_to_deadline_days', 'staff_pick', 'pledged', 'backers_count', 'spotlight', 'goal']]
     return model.predict(X_features)
